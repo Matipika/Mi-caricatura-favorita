0fe1877761bb066eb9c66f1be99c0c6795820a1b
Matipika <pikachuranger14@gmail.com>
2024-08-27 16:40:33 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function changeImageOver()
+{
+  document.getElementById("cartoon").src="https://64.media.tumblr.com/0e3c95ac8fa5f39bf91e4433d491bb6c/7f3033b231abab3a-83/s640x960/eff8b6c76acc5e6014f06a1bd3d3543c8dec1cb3.png"
+}
+
+function changeImageLeave()
+{
+document.getElementById("cartoon").src="https://images.justwatch.com/poster/241633683/s592/temporada-3"}
